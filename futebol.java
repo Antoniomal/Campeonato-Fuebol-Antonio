1641ddcb15b0ac84ab58cf22de499761e80d239a
Antoniomal <162126312+Antoniomal@users.noreply.github.com>
2024-04-11 08:25:53 -0300

    Add files via upload

--- futebol.java
+++ futebol.java
@@ -1,10 +1,51 @@
 package condicional;
 
+import java.util.Scanner;
+
 public class futebol {
+    public static void main(String[] args) {
+        final int qtdeTimes = 5;
+        final int qtdeJogadores = 11;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int qtdeMenor18 = 0;
+        int qtdeAcima80Kg = 0;
 
-	}
+        Scanner sc = new Scanner(System.in);
 
+        for (int i = 1; i <= qtdeTimes; i++) {
+            float mediaIdade = 0;
+            float mediaAltura = 0;
+            int contMediaIdade = 0;
+            int contMediaAltura = 0;
+
+            for (int j = 1; j <= qtdeJogadores; j++) {
+                System.out.println("Informe a idade do jogador: ");
+                int idade = sc.nextInt();
+                System.out.println("Informe a altura do jogador: ");
+                double altura = sc.nextDouble();
+                System.out.println("Informe o peso do jogador: ");
+                double peso = sc.nextDouble();
+
+                if (idade < 18) {
+                    qtdeMenor18++;
+                }
+                if (peso > 80) {
+                    qtdeAcima80Kg++;
+                }
+
+                mediaIdade += idade;
+                contMediaIdade++;
+                mediaAltura += altura;
+                contMediaAltura++;
+            }
+
+            System.out.printf("A média de idade do time %d é %.2f\n", i, mediaIdade / contMediaIdade);
+            System.out.printf("A altura média do time %d é %.2f\n", i, mediaAltura / contMediaAltura);
+        }
+
+        System.out.printf("O campeonato apresenta %d jogadores menores que 18 anos\n", qtdeMenor18);
+        System.out.printf("O campeonato apresenta %d jogadores acima de 80 Kg\n", qtdeAcima80Kg);
+
+        sc.close();
+    }
 }
